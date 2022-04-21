6c0185f62afaedc267de8ce3fcb45b99aa046d24
SaiKrishna6699 <saikrishnarajagopal@gmail.com>
2022-04-21 14:13:50 +0530

    Adding APP 1,2

---
+++ Phase4/Assisted Practice Projects/PPA3/a1_angular-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template:'<product-list></product-list>',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'a1_angular-app';
+}
---
+++ Phase4/Assisted Practice Projects/PPA3/a2-Property-Binding/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<product-list><AppComponent/product-list>',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'a2-Property-Binding';
+}
