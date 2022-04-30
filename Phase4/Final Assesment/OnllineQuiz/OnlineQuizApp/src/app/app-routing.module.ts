9258339e531817b45de76c6b447e2e2fef03d5ab
SaiKrishna6699 <saikrishnarajagopal@gmail.com>
2022-04-30 20:19:13 +0530

    Delayed adding Phase 4 Final

---
+++ Phase4/Final Assesment/OnllineQuiz/OnlineQuizApp/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionComponent } from './question/question.component';
+import { ResultComponent } from './result/result.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'welcome', pathMatch:"full"},
+  {path:'welcome', component:WelcomeComponent},
+  {path:'question', component:QuestionComponent},
+  {path: 'result', component:ResultComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
